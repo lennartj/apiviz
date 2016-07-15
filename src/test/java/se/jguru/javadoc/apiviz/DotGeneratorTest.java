df2fa28d450de9e7a5fea2bb7fa76560cf06b8b8
Lennart Jörelid <lj@jguru.se>
2016-07-15 14:57:51 +0200

    Splitting up the visualizer into API and Doclet projects.

---
+++ src/test/java/se/jguru/javadoc/apiviz/DotGeneratorTest.java
@@ -0,0 +1,21 @@
+package se.jguru.javadoc.apiviz;
+
+import org.antlr.v4.tool.DOTGenerator;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:lj@jguru.se">Lennart J&ouml;relid</a>, jGuru Europe AB
+ */
+public class DotGeneratorTest {
+
+    @Test
+    public void validateCreatingTrivialDiagramFile() {
+
+        // Assemble
+        final DOTGenerator unitUnderTest = new DOTGenerator()
+
+        // Act
+
+        // Assert
+    }
+}
